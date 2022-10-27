c631b9bb0c7f043a3ebcf9bd08315e72d4524f4b
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-27 18:31:35 +0300

    first task complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,30 @@
 let getEl = selector => document.querySelector(selector);
-getEl(button);
-console.log(getEl);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = getEl('[data-start]');
+const btnStop = getEl('[data-stop]');
+const body = document.body;
+
+btnStart.addEventListener('click', clickStart);
+btnStop.addEventListener('click', clickStop);
+
+btnStop.setAttribute('disabled', true);
+let interval = null;
+const DELAY = 1000;
+
+function clickStart() {
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+}
+
+function clickStop() {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+  clearInterval(interval);
+}
