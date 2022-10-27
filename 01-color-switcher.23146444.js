0c5f6981aaefd4bf3a4bdcfc334d030b61b646a5
Pobedynskyj <105819145+Pobedynskyj@users.noreply.github.com>
2022-10-27 12:59:52 +0000

    Deploying to gh-pages from @ Pobedynskyj/goit-js-hw-09@4d662243b8e977ad173d1202531039ce3f22e20a 🚀

---
+++ 01-color-switcher.23146444.js
@@ -0,0 +1,2 @@
+let e=e=>document.querySelector(e);e(button),console.log(e);
+//# sourceMappingURL=01-color-switcher.23146444.js.map
---
+++ 01-color-switcher.3f34f57e.js
@@ -0,0 +1,2 @@
+!function(){var n=function(n){return document.querySelector(n)};n(button),console.log(n)}();
+//# sourceMappingURL=01-color-switcher.3f34f57e.js.map
