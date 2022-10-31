be5776276b3d50014a2583bedbd218f574797a99
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-11-01 00:00:35 +0200

    add let

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+import Notiflix, { Notify } from 'notiflix';
+
+let form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  let delay = event.currentTarget[0].value;
+  console.log(delay);
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
